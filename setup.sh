5030c382f78865b10f1acb0991da1fd41da7f1b7
GTr487 <Gunther.molina.487@gmail.com>
2025-05-02 21:38:06 -0300

    feat: archlinux config

--- setup.sh
+++ setup.sh
@@ -10,38 +10,64 @@ show_version() {
     echo -e "--------------------------------------------------------\n                                                  v$VERSION\n--------------------------------------------------------"
 }
 
-install_ansible() {
-    if ! command -v ansible &> /dev/null; then
-        echo "# Installing Ansible..."
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Could not detect distro."
+        exit 1
+    fi
+}
 
+install_ansible_arch() {
+    if ! command -v ansible &> /dev/null; then
+        echo "# Installing Ansible on Arch Linux..."
+
+        sudo pacman -Syu --noconfirm ansible
+    else
+        echo "# Ansible is already installed."
+    fi
+}
+
+install_ansible_debian() {
+    if ! command -v ansible &> /dev/null; then
+        echo "# Installing Ansible on Debian based system..."
+
+        sudo apt update
         sudo apt install -y ansible
     else
         echo "# Ansible is already installed."
     fi
 }
 
-apt_update() {
-    if [ ! -f /var/lib/apt/periodic/update-success-stamp ] || \
-        [ $(find /var/lib/apt/periodic/update-success-stamp -mtime +1 -print) ]; then
-            echo "# Running 'apt update'..."
-
-            sudo apt update
-        else
-            echo "# 'apt update' is not necessary; it was run recently."
-    fi
-}
-
 execute_playbook() {
-    echo "# Running playbook..."
-    ansible-playbook -i localhost, -c local playbook.yml --ask-become-pass
+    echo "# Running $PLAYBOOK..."
+    ansible-playbook -i localhost, -c local "$PLAYBOOK" --ask-become-pass
 }
 
+# MAIN
 show_header
 echo
 show_version
 echo
 echo
 
-apt_update
-install_ansible
+detect_distro
+
+case "$DISTRO" in
+    arch)
+        PLAYBOOK="playbooks/arch_playbook.yml"
+        install_ansible_arch
+        ;;
+    ubuntu | debian)
+        PLAYBOOK="playbooks/debian_playbook.yml"
+        install_ansible_debian
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        exit 1
+        ;;
+esac
+
 execute_playbook
