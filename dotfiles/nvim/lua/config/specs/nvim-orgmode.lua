baddf7cd78ab98da52706f8abd6f70ea70b5d3b1
GTr487 <g@g.g>
2025-06-13 18:22:28 -0300

    feat: NvimOrgmode

--- dotfiles/nvim/lua/config/specs/nvim-orgmode.lua
+++ dotfiles/nvim/lua/config/specs/nvim-orgmode.lua
@@ -1,25 +1,43 @@
 return {
-	"nvim-orgmode/orgmode",
-	event = "VeryLazy",
-	ft = { "org" },
-	config = function()
-		-- Setup orgmode
-		require("orgmode").setup({
-			org_agenda_files = "~/orgfiles/**/*",
-			org_default_notes_file = "~/orgfiles/refile.org",
-			org_todo_keywords = { "TODO", "DOING", "|", "DONE" },
-			org_todo_keyword_faces = {
-				DOING = ":foreground #00FFC8 :weight bold",
-			},
-		})
+  "nvim-orgmode/orgmode",
+  event = { "BufReadPre", "BufNewFile" },
+  ft = { "org" },
+  config = function()
+    local home = os.getenv("HOME")
+    local org_path = home .. "/worknotes/org"
 
-		vim.keymap.set("n", "<leader>of", ":Telescope find_files cwd=~/orgfiles<CR>")
+    -- Autoreload si hay cambios externos (Syncthing, etc.)
+    vim.o.autoread = true
+    vim.cmd([[
+      autocmd FocusGained,BufEnter * checktime
+    ]])
 
-		-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-		-- add ~org~ to ignore_install
-		-- require('nvim-treesitter.configs').setup({
-		--   ensure_installed = 'all',
-		--   ignore_install = { 'org' },
-		-- })
-	end,
+    -- Keybindings globales
+    vim.keymap.set("n", "<leader>of", ":Telescope find_files cwd=" .. org_path .. "<CR>", { desc = "Buscar en vault/org" })
+
+    vim.keymap.set("n", "<leader>od", function()
+      if vim.fn.winnr('$') == 1 then
+        vim.cmd("vsplit")
+      else
+        vim.cmd("rightbelow vsplit")
+      end
+      vim.cmd("e " .. org_path)
+    end, { desc = "Abrir directorio org en split lateral" })
+
+
+    require("orgmode").setup({
+      org_agenda_files = { org_path .. "/**/*" },
+      org_default_notes_file = org_path .. "/refile.org",
+
+      -- Estados personalizados
+      org_todo_keywords = { "TODO", "DOING", "WAITING", "|", "DONE", "CANCELLED" },
+      org_todo_keyword_faces = {
+        DOING     = ":foreground #00FFC8 :weight bold",
+        WAITING   = ":foreground #FFD700 :weight bold",
+        CANCELLED = ":foreground #888888 :slant italic",
+      },
+
+      win_split_mode = "vertical",  -- abre capturas/agendas en split automático
+    })
+  end,
 }
--- dotfiles/tmux/layouts/cml-frontend-claro.session.sh
+++ dotfiles/tmux/layouts/cml-frontend-claro.session.sh
@@ -3,16 +3,30 @@ session_root "~/workspace/claro/cml/repositories"
 if initialize_session "cml-frontend-claro"; then
 
   new_window "Editor"
-  run_cmd "nvim cml-frontend-claro.git"
+  run_cmd "cd cml-frontend-claro.git"
+  run_cmd "nvim ."
 
   split_v 5
   run_cmd "cd cml-frontend-claro.git"
+  run_cmd "HISTFILE=/tmp/cml-frontend-claro_w1p2.zsh_history zsh"
+  run_cmd "cls"
   send_keys "npm start"
 
   split_h 50 
   run_cmd "cd cml-business-service.git"
+  run_cmd "HISTFILE=/tmp/cml-frontend-claro_w1p3.zsh_history zsh"
+  run_cmd "cls"
   send_keys "mvn spring-boot:run -Pdev-ar"
 
+  new_window "Terminals"
+  run_cmd "cd cml-frontend-claro.git"
+  run_cmd "git status"
+
+  split_h 50 
+  run_cmd "cd cml-business-service.git"
+  run_cmd "git status"
+
+  select_window 1
   select_pane 1
 
 fi
