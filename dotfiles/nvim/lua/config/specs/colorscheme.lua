adccde96a27ef8eb65718532cf53f61514ca7cd9
gtr487 <32377281+GTr487@users.noreply.github.com>
2025-06-17 00:17:31 -0300

    Arch: config

--- dotfiles/nvim/lua/config/specs/colorscheme.lua
+++ dotfiles/nvim/lua/config/specs/colorscheme.lua
@@ -5,7 +5,9 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
+      vim.o.background = "dark"
 			vim.cmd([[colorscheme cyberpunk]])
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
 		end,
 	},
 }
